977248ae49c5831fcf7e3870c9950d4618f3b9e0
Dervis <dervishalilcevic@gmail.com>
2020-06-28 23:07:47 +0200

    Add JS

--- scripts/script.js
+++ scripts/script.js
@@ -10,12 +10,16 @@ let playerNamePrompt;
 do {
     playerNamePrompt = prompt("Enter your name: ");
     playerName.innerHTML = playerNamePrompt;
+    playerName = playerNamePrompt;
 } 
 while( playerNamePrompt == null || playerNamePrompt.length == 0 );
 
 
 
 let counter = 1;
+
+let resultComp = document.getElementById("resultComp");
+let resultPlayer = document.getElementById("resultPlayer");
 
 let counterPlayer = 0;
 let counterComputer = 0;
@@ -66,9 +70,11 @@ function potez(){
     else if( playerPotez == "rock" && computerPotez ==  "scissors" || playerPotez == "paper" && computerPotez == "rock" || playerPotez == "scissors" && computerPotez == "paper"){
         demo.innerHTML = playerName + " win";
         counterPlayer++;
+        resultPlayer.innerHTML = counterPlayer;
     }else{
         demo.innerHTML = "Computer win this round";
         counterComputer++;
+        resultComp.innerHTML = counterComputer;
     }
 
     if(counter > 4){
@@ -84,7 +90,11 @@ function potez(){
             alert(playerName + " wins with resultat " + counterPlayer + ":" + counterComputer);
             counterPlayer = 0;
             counterComputer = 0;
+            resultPlayer = 0;
+            resultComp = 0;
         }
+        resultComp.innerHTML = 0;
+        resultPlayer.innerHTML = 0;
         counter = 0;
     }
     round.innerHTML = "<u>" + ++counter +"</u>";
